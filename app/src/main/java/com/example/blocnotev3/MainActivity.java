661298451d2db950547e8bd3479c06ab7d2a1432
utilisateur <utilisateur>
2019-01-24 23:05:22 +0100

    - V5 - Auth avec ecriture dans la BDD

--- app/src/main/java/com/example/blocnotev3/MainActivity.java
+++ app/src/main/java/com/example/blocnotev3/MainActivity.java
@@ -2,20 +2,30 @@ package com.example.blocnotev3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.blocnotev3.Helper.FileHelper;
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
 
-public class  MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private static final int RC_SIGN_IN = 123;
 
     // Déclaration FirebaseAuth
     private FirebaseAuth mAuth;
+
+    // Déclaration Boutton
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,36 @@ public class  MainActivity extends AppCompatActivity implements View.OnClickList
 
         // Initialisation de l'instance "FirebaseAuth"
         mAuth = FirebaseAuth.getInstance();
+
+        Button button = findViewById(R.id.main_activity_button_login);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                this.startSignInActivity();
+
+            }
+
+            // Launch Sign-In Activity
+            private void startSignInActivity() {
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                // Création d'une activité de connexion géré par firebase UI
+                                .createSignInIntentBuilder()
+                                .setTheme(R.style.LoginTheme) // Personnalisation de l'écran
+                                // Ajout des moyens d'authentification
+                                // Methode auth par email
+                                .setAvailableProviders(
+                                        Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(), // EMAIL
+                                                new AuthUI.IdpConfig.GoogleBuilder().build())) // GOOGLE
+                                .setIsSmartLockEnabled(false, true)
+                                .setLogo(R.drawable.ic_logo_auth)
+                                .build(),
+                        RC_SIGN_IN);
+            }
+
+        });
+
     }
 
     @Override
@@ -34,8 +74,11 @@ public class  MainActivity extends AppCompatActivity implements View.OnClickList
         updateUI(currentUser);
     }
 
-    private void updateUI(FirebaseUser currentUser) {
+
+
+    private void updateUI(FirebaseUser user) {
     }
+
 
     @Override
     public void onClick(View v) {
