8a4814162bfd3389af8e168006ccd7f74141695c
utilisateur <utilisateur>
2019-01-28 18:41:10 +0100

    - V8 - login ok / changement de bouton une fois logé ok / accès à la création d'une note ok

--- app/src/main/java/com/example/blocnotev3/MainActivity.java
+++ app/src/main/java/com/example/blocnotev3/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.blocnotev3;
 
 import android.content.ContentProvider;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,11 +12,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
+
 
 import com.example.blocnotev3.Auth.ProfilActivity;
 import com.example.blocnotev3.Base.NotesListe;
 import com.example.blocnotev3.Helper.FileHelper;
+import com.example.blocnotev3.Helper.Helper;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -27,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     // Déclaration FirebaseAuth
     private FirebaseAuth mAuth;
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +48,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mAuth = FirebaseAuth.getInstance();
 
         Button button = findViewById(R.id.main_activity_button_login);
+        // On ajoute le paramétre "button" à la méthode appelée
+        this.updateUIWhenResuming(button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Si l'utilisateur est connecté, alors on passe sur l'activité suivante
                 // Start appropriate activity
-                if (this.isCurrentUserLogged()) {
+
+                // this.startSignInActivity();
+
+                if (this.isCurrentUserLogged())
+                {
                     this.startNotesListeActivity();
                 } else {
                     this.startSignInActivity();
                 }
-
             }
 
             // L'utilisateur actuel est il connecté ?
@@ -59,12 +75,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // Récupère l'utilisateur actuellement connecté à l'app
             protected FirebaseUser getCurrentUser() {
                 return FirebaseAuth.getInstance().getCurrentUser();
-            }
-
-            // Launching Profile Activity
-            private void startNotesListeActivity() {
-                Intent intent = new Intent(MainActivity.this, NotesListe.class);
-                startActivity(intent);
             }
 
             // Launch Sign-In Activity
@@ -86,8 +96,83 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         RC_SIGN_IN);
             }
 
-        });
+            // Launching "NotesListeActivity"
+            private void startNotesListeActivity() {
+                Intent intent = new Intent(MainActivity.this, NotesListe.class);
+                startActivity(intent);
+            }
 
+        });
+    }
+
+    // Update UI when activity is resuming : mise à jour du texte du "button" en fonction du statut de l'utilisateur
+    private void updateUIWhenResuming(Button button) {
+        button.setText
+                (this.isCurrentUserLogged() ? getString(R.string.button_login_text_logged) : getString(R.string.button_login_text_not_logged));
+    }
+
+
+    // L'utilisateur actuel est il connecté ?
+    protected Boolean isCurrentUserLogged() {
+        return (this.getCurrentUser() !=null);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Handle (manipuler) SignIn Activity response on activity result
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    // Method that handles (manipuler) response after SignIn Activity close
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                this.createUserInFirestore();
+                showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
+                }
+            }
+        }
+    }
+
+    // Show Snack Bar with a message
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message) {
+        Snackbar.make((findViewById(R.id.main_activity_coordinator_layout)), message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    // Http request that create user in firestore
+    private void createUserInFirestore(){
+        if (this.getCurrentUser() != null){
+            String urlPicture = (this.getCurrentUser().getPhotoUrl() != null ? this.getCurrentUser().getPhotoUrl().toString() : null);
+            String username = this.getCurrentUser().getDisplayName();
+            String uid = this.getCurrentUser().getUid();
+
+            Helper.createUser(uid, username, urlPicture).addOnFailureListener(this.onFailureListener());
+        }
+    }
+
+    protected OnFailureListener onFailureListener(){
+        return new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), getString(R.string.error_unknown_error), Toast.LENGTH_LONG).show();
+            }
+        };
+    }
+
+    // Récupère l'utilisateur actuellement connecté à l'app
+    protected FirebaseUser getCurrentUser() {
+        return FirebaseAuth.getInstance().getCurrentUser();
     }
 /*
     @Override
@@ -135,6 +220,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
     }
-
-
  */
