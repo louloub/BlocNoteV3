0f35e405935c4bd9d771be22c3e9d0135b36efd9
utilisateur <utilisateur>
2019-02-14 15:18:35 +0100

    V3 - INTENT sur le click d'un ITEM ok (activité CreateNote) Next step : pouvoir modifier le texte de la note sur laquelle on click depuis cette activité

--- app/src/main/java/com/example/blocnotev3/Adapter/AdapterListe.java
+++ app/src/main/java/com/example/blocnotev3/Adapter/AdapterListe.java
@@ -1,34 +1,24 @@
 package com.example.blocnotev3.Adapter;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.blocnotev3.MainActivity;
-import com.example.blocnotev3.Notes;
+import com.example.blocnotev3.Base.NotesListe;
+import com.example.blocnotev3.CreateNote;
 import com.example.blocnotev3.R;
-
 import java.util.ArrayList;
-import java.util.List;
 
-import static com.facebook.share.widget.ShareDialog.show;
-import static com.firebase.ui.auth.AuthUI.getApplicationContext;
-import static java.security.AccessController.getContext;
+import static android.support.v4.content.ContextCompat.startActivity;
 
 public class AdapterListe extends ArrayAdapter<String> {
 
-    private MainActivity activity;
+    private AdapterListe activity;
 
     // Constructeur Adapter
     public AdapterListe(Context mContext, ArrayList<String> mOriginalValues){
@@ -46,27 +36,20 @@ public class AdapterListe extends ArrayAdapter<String> {
         titleTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder myPopup = new AlertDialog.Builder(activity);
-                myPopup.setTitle("Salut");
-                myPopup.setMessage("Salut les gens c'est ton téléphone qui te parle");
-                myPopup.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(), "Vous avez cliquez sur Oui", Toast.LENGTH_SHORT).show();
-                    }
-                });
 
-                myPopup.setNegativeButton("Non", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(), "Vous avez cliquez sur Non", Toast.LENGTH_SHORT).show();
-                    }
+                Intent intent = new Intent(getContext(), CreateNote.class);
+                startActivity(getContext(),intent, Bundle.EMPTY);
 
-                });
 
-                myPopup.show();
             }
+
+
+
+
         });
+
+
+
 
         // Récupérer la postiion de chaque item
         String item = getItem(position);
@@ -75,4 +58,13 @@ public class AdapterListe extends ArrayAdapter<String> {
 
         return convertView;
     }
+/*
+    public void startActivity(Intent intent) {
+        this.startActivity(intent);
+    }
+
+    Intent intent = new Intent(getContext(), CreateNote.class);
+                startActivity(this, intent, Bundle options);
+*/
+
 }
